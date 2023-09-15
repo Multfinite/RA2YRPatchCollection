f3666cf9821359076a5d9f3783df2d41fb84146d
Multfinite <Multfinite@gmail.com>
2023-09-15 18:14:55 +0300

    Tweaks: demotruck deploy fire

--- generate.sh
+++ generate.sh
@@ -13,7 +13,7 @@ Output=`cat settings.json | jq --raw-output '.Output'`
 
 eval "\"${INIMerger}\" -tron \"${Output}/gm_invasion_vanilla.ini\" edit_normandy_control_points.ini"
 
-eval "\"${INIMerger}\" -tron \"${Output}/gm_invasion.ini\" edit_proxy_mcv.ini edit_undestroyable_flags.ini edit_advanced_flags.ini edit_flags.ini edit_lowcost_infantry.ini fix_bfrt.ini fix_grand_cannon.ini fix_siege_chopper.ini edit_repairable_bunkers.ini edit_paradrop.ini edit_transport_zeppelin_for_scripts.ini edit_capturable_lax.ini edit_secret_lab.ini edit_outpust_allow_mcv_conopt.ini edit_ext_occupy.ini edit_transport_via_factory.ini edit_c4_tech.ini edit_normandy_control_points.ini buff_paradrop.ini buff_tesla_coil.ini buff_v3.ini buff_transport.ini buff_bunkers.ini"
+eval "\"${INIMerger}\" -tron \"${Output}/gm_invasion.ini\" edit_tweaks.ini edit_proxy_mcv.ini edit_undestroyable_flags.ini edit_advanced_flags.ini edit_flags.ini edit_lowcost_infantry.ini fix_bfrt.ini fix_grand_cannon.ini fix_siege_chopper.ini edit_repairable_bunkers.ini edit_paradrop.ini edit_transport_zeppelin_for_scripts.ini edit_capturable_lax.ini edit_secret_lab.ini edit_outpust_allow_mcv_conopt.ini edit_ext_occupy.ini edit_transport_via_factory.ini edit_c4_tech.ini edit_normandy_control_points.ini buff_paradrop.ini buff_tesla_coil.ini buff_v3.ini buff_transport.ini buff_bunkers.ini"
 eval "\"${INIMerger}\" -tron \"${Output}/gm_invasion_fleet_patch.ini\" \"${Output}/gm_invasion.ini\" edit_fleet_patch.ini"
 eval "\"${INIMerger}\" -tron \"${Output}/gm_invasion_yss.ini\" \"${Output}/gm_invasion.ini\" mode_soviet_yuri.ini"
 eval "\"${INIMerger}\" -tron \"${Output}/gm_invasion_fleet_patch_yss.ini\" \"${Output}/gm_invasion_fleet_patch.ini\" mode_soviet_yuri.ini"
